356309886772d89194f9aa5da19559ebefc57c0b
shanggu <shanggu@shanggu-macmini.local>
2016-05-20 14:22:53 +0800

    Initial Commit

---
+++ LZCountdownButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZCountdownButton
+//
+//  Created by shanggu on 16/5/20.
+//  Copyright © 2016年 shanggu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LZCountdownButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LZCountdownButton
+//
+//  Created by shanggu on 16/5/20.
+//  Copyright © 2016年 shanggu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
